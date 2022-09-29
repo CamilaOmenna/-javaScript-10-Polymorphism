a55f11be5fe44f6be0f66dd18193d93395c5def2
Camila Omenna <112710301+CamilaOmenna@users.noreply.github.com>
2022-09-29 01:08:24 -0300

    Add files via upload

---
+++ animal.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports,"__esModule", { value: true });
+exports.Animal = void 0;
+var Animal = /** @class */ (function () {
+    function Animal(nome,idade,emitirsom) {
+        this.nome = nome;
+        this.idade = idade;
+        this.emitirSom = emitirsom;
+    }
+    return Animal;
+}());
+exports.Animal = Animal;
